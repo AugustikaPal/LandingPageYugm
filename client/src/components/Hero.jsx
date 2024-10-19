654cd869a49243093173a8a2804699ce4551fcf9
Neha20072002 <nehakatiyar2007@gmail.com>
2024-10-19 16:25:48 +0530

    ui changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-          Unleash the power of AI within Yugm. <br/>Boost productivity using the open AI chat app.
+          Unleash the power of Smarter Knowledge Access with Yugm. <br/>Boost productivity using the AI chat app.
           </p>
          
         </div>
@@ -46,13 +46,18 @@ const Hero = () => {
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
 
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
-                <img
-                  src={robot}
-                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
-                  width={1024}
-                  height={490}
-                  alt="AI"
-                />
+  <video
+    className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+    width={1024}
+    height={490}
+    autoPlay
+    loop
+    muted
+    playsInline
+  >
+    <source src={robot} type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
 
                 <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
